9b7f324a112bf418d158cbc63ddac670557edfe6
Vero Conesa <vero.gconesa@gmail.com>
2023-03-04 23:07:13 +0100

    🆕 feat changed color

--- src/utils.js
+++ src/utils.js
@@ -2,6 +2,6 @@ import { outro } from '@clack/prompts'
 import colors from 'picocolors'
 
 export function exitProgram ({ code = 0, msg = 'Exit: No files to commit' } = {}) {
-  outro(colors.blue(msg))
+  outro(colors.yellow(msg))
   process.exit(code)
 }
